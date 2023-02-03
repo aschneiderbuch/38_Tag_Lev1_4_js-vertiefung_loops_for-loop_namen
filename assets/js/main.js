2eb8a70c65a36279559b817fac727d58c89a90c7
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 13:03:26 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,15 @@ console.log("test");
 
 Frage
 
+array namen = 
 
+for Schleife alle Namen anzeigen lassen
 */
+
+
+
+let namen = ["Freddy","Steffen", "Finn", "Julia","Franzi", "Christian" , "Sergio"];
+
+for (let runden = 0; runden < namen.length ; runden = runden + 1){
+    console.log(namen[runden]);
+}
